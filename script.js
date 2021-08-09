a64b884bece0c9072e9a5908368601bbf21335db
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-08-09 15:44:45 -0500

    Add dinamic creation of books

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-let books = {};
-
-const newBook = function (book, author) {};
+let booksArray = [];
+let Book = function (title, author) {
+  this.title = title;
+  this.author = author;
+};
+let form = document.querySelector("#submitBttn");
+form.addEventListener("click", (event) => {
+  let inputs = document.querySelectorAll("#bookForm input[type='text']");
+  let titleValue = inputs[0].value;
+  let authorValue = inputs[1].value;
+  console.log(titleValue, authorValue);
+  // Create book object
+  let newBook = new Book(titleValue, authorValue);
+  booksArray.push(newBook);
+  console.log(booksArray);
+  let divSection = document.createElement("div");
+  divSection.innerHTML =
+    "<p>Title</p>" +
+    "<p>Author</p>" +
+    `<button class = 'removeButton'>Remove</button>`;
+  divSection.id = `${booksArray.length - 1}`;
+  document.body.appendChild(divSection);
+  let buttonId = document.querySelectorAll(".removeButton");
+  console.log(buttonId);
+  for (let i = 0; i < buttonId.length; i = i + 1) {
+    buttonId[i].addEventListener("click", function () {
+      let sectionId = document.getElementById(`${i}`);
+      sectionId.remove();
+    });
+  }
+});
